725415b99acf932d220204e2da8925bccb9be1b9
xwjiang <jiangxinwei1997@163.com>
2021-11-14 14:36:52 +0800

    开始一段崭新的体验与坚持

---
+++ BaseTest/src/main/java/HelloAlgorithm.java
@@ -0,0 +1,14 @@
+/**
+ * @Description:
+ * @author: xwjiang
+ * @Version: 1.0
+ * @date 2021/11/14 14:00
+ */
+public class HelloAlgorithm {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Algorithm,你好，算法！");
+        System.out.println("你好，崭新的我！！！");
+    }
+}
